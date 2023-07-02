98cc4d3284c4fbfa66e447f33151b22cdd0d6313
kanar <romankana01@gmail.com>
2023-07-02 19:57:21 +0200

    Moved the problems into problems.pl and added some structure checking.

--- src/sudoku_solver.pl
+++ src/sudoku_solver.pl
@@ -1,34 +1,15 @@
+
+:- module(kanar_sudoku, [solve_sudoku/1]).
+
+:- use_module(problems).
 :- use_module(library(clpfd)).
 
-problem(1, P) :- P = [[1,_,_,8,_,4,_,_,_],
-                      [_,2,_,_,_,_,4,5,6],
-                      [_,_,3,2,_,5,_,_,_],
-                      [_,_,_,4,_,_,8,_,5],
-                      [7,8,9,_,5,_,_,_,_],
-                      [_,_,_,_,_,6,2,_,3],
-                      [8,_,1,_,_,_,7,_,_],
-                      [_,_,_,1,2,3,_,8,_],
-                      [2,_,5,_,_,_,_,_,9]].
+% a problem is list of lists so lets work with them like so
+% steps lets work only with 9x9; the NxN should be possible to be done later on
 
 
-problem(2, P) :- P = [[_,_,2,_,3,_,1,_,_],
-                      [_,4,_,_,_,_,_,3,_],
-                      [1,_,5,_,_,_,_,8,2],
-                      [_,_,_,2,_,_,6,5,_],
-                      [9,_,_,_,8,7,_,_,3],
-                      [_,_,_,_,4,_,_,_,_],
-                      [8,_,_,_,7,_,_,_,4],
-                      [_,9,3,1,_,_,_,6,_],
-                      [_,_,7,_,6,_,5,_,_]].
-
-problem(3, P) :- P = [[1,_,_,_,_,_,_,_,_],
-                      [_,_,2,7,4,_,_,_,_],
-                      [_,_,_,5,_,_,_,_,4],
-                      [_,3,_,_,_,_,_,_,_],
-                      [7,5,_,_,_,_,_,_,_],
-                      [_,_,_,_,_,9,6,_,_],
-                      [_,4,_,_,_,6,_,_,_],
-                      [_,_,_,_,_,_,_,7,1],
-                      [_,_,_,_,_,1,_,3,_]].
-
-solve_sudoku(Problem, Solution).
+solve_sudoku(Problem) :- 
+    maplist(same_length(Problem), Problem),
+    transpose(Problem, Transposed), length(Problem, N), length(Transposed, N).
+    
+    
