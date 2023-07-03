704283643a81696753235c8c9c9285c29630cb22
kanar <romankana01@gmail.com>
2023-07-03 16:49:33 +0200

    Formatting

--- documentation/dev_documentation.md
+++ documentation/dev_documentation.md
@@ -2,14 +2,20 @@
 
 ## **1.1 Assignment**
 
-Create a Prolog program with predicates that solve sudoku problems.
+Create a Prolog program with predicates that solve sudoku problems of size $N^2{\times}N^2$, where $N\in\mathbb{N}$.
 
 ## **1.2 Background**
+
 ## **1.3 Scope**
+
 ## **1.4 TODOs**
+
 # **2 Description**
+
 ## **2.1 File structure**
+
 ## 2.2 
+
 # **3 Requirements**
 
 ## **3.1 Software**
--- src/domains.pl
+++ src/domains.pl
@@ -4,11 +4,11 @@
 
 % static 9x9 domains
 
-sudoku_domain(X) :- X #> 0, X #< 10. % 1..9
+sudoku_domain(Var) :- Var #> 0, Var #< 10. % 1..9
 
-odd_domain(X) :- X #>= 1, X #=< 18, X mod 2 #= 1.
+odd_domain(Var) :- Var #>= 1, Var #=< 18, Var mod 2 #= 1.
 
-even_domain(X) :- X #>= 1, X #=< 18, X mod 2 #= 0.
+even_domain(Var) :- Var #>= 1, Var #=< 18, Var mod 2 #= 0.
 % dynamic (N^2) x (N^2) domains, where N there are N^2 subblocks of size NxN
 
-sudoku_dynamic(X, Min, Max) :- X ins Min..Max.
+sudoku_dynamic(Var, Min, Max) :- Var = Min..Max.
